877a9a50ebedbc4e3f9dbc213e6b014d407fb4b4
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-02 15:18:59 +0200

    feat: execute stdout & stderr

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,49 @@
 use anyhow::{Context, Result};
+use clap::{Parser, Subcommand};
+use std::io::{self, Write};
+use std::process::{Command, Stdio};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Run docker command
+    Run {
+        /// The arguments of executed commands
+        args: Vec<String>,
+    },
+}
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    println!("Logs from your program will appear here!");
+    let args = Args::parse();
 
-    let args: Vec<_> = std::env::args().collect();
-    let command = &args[3];
-    let command_args = &args[4..];
-    let output = std::process::Command::new(command)
-        .args(command_args)
-        .output()
-        .with_context(|| {
-            format!(
-                "Tried to run '{}' with arguments {:?}",
-                command, command_args
-            )
-        })?;
-
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
-        std::process::exit(1);
+    match args.command {
+        Commands::Run { args } => {
+            let command = &args[1];
+            let command_args = &args[2..];
+            let output = Command::new(command)
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .args(command_args)
+                .output()
+                .with_context(|| {
+                    format!(
+                        "Tried to run '{}' with arguments {:?}",
+                        command, command_args
+                    )
+                })?;
+            if !output.status.success() {
+                std::process::exit(1);
+            }
+            io::stdout().write_all(&output.stdout)?;
+            io::stderr().write_all(&output.stderr)?;
+        }
     }
 
     Ok(())
